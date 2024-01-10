a109a9239f853ae325b314c005e5cb914e07f6f8
Tom <->
2024-01-10 23:57:45 +0100

    Created home test controller

---
+++ ZTPAI 3/src/test/java/com/example/ZTPAI/ZtpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZTPAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtpaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
