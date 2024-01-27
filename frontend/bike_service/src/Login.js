ff72e94ab89b811d12e8a4d721552284f71f363a
Tom <->
2024-01-27 12:01:37 +0100

    Connected login into backend

--- frontend/bike_service/src/Login.js
+++ frontend/bike_service/src/Login.js
@@ -4,56 +4,34 @@ import './Login.css';
 import bike from './Assets/bike.svg';
 
 const LoginPage = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const [errorMessage, setErrorMessage] = useState('');
-
   const navigate = useNavigate();
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
+    // Wykonaj request do serwera
     try {
-      // Wysłanie danych do serwera przy użyciu funkcji fetch
-      const response = await fetch('http://localhost:8088/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(`http://localhost:8088/v1/user/getByEmail?email=${email}`);
+      if (response.status === 404) {
+        setError('Nie znaleziono użytkownika!');
+      } else if (response.status === 200) {
+        const user = await response.json();
 
-      if (response.ok) {
-        // Pobranie danych z odpowiedzi serwera
-        const users = await response.json();
-
-        // Sprawdzenie, czy email i hasło się zgadzają
-        const user = users.find((u) => u.email === formData.email && u.password === formData.password);
-
-        if (user) {
-          // Zalogowano pomyślnie, przekierowanie do "abc"
-          navigate('/abc');
+        if (user.password === password) {
+          setError('OK');
+          console.log("Passwords match!")
         } else {
-          // Nieprawidłowe dane logowania
-          setErrorMessage('Invalid email or password');
+          setError('Niepoprawne hasło!');
         }
       } else {
-        // Błąd podczas próby logowania
-        setErrorMessage('Failed to log in');
+        setError('Błąd logowania. Spróbuj ponownie.');
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Błąd logowania:', error);
+      setError('Błąd logowania. Spróbuj ponownie.');
     }
   };
 
@@ -68,17 +46,29 @@ const LoginPage = () => {
       <h1>Login</h1>
 
       <div className="message" style={{ color: 'red' }}>
-        {errorMessage}
+        {error}
       </div>
 
       <label>
-        <b>Username</b>
-        <input type="text" placeholder="Email" name="email" value={formData.email} onChange={handleInputChange} />
+        <b>Email</b>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </label>
 
       <label>
         <b>Password</b>
-        <input type="password" placeholder="Password" name="password" value={formData.password} onChange={handleInputChange} />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </label>
 
       <button className="login-button" type="submit" onClick={handleLogin}>
