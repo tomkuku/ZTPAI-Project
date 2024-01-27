b92390d4e88a152656105cf11d5bcbe1afa3d6da
Tom <->
2024-01-27 11:33:13 +0100

    Added support for CORS

---
+++ backend-service/src/main/java/com/tkukulka/ztpai/manager/service/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.tkukulka.ztpai.manager.service;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("localhost:3000/signup", "localhost:3000/login")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "Content-Type", "Accept")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- backend-service/src/main/java/com/tkukulka/ztpai/manager/service/ZtpaiServiceApplication.java
+++ backend-service/src/main/java/com/tkukulka/ztpai/manager/service/ZtpaiServiceApplication.java
@@ -2,7 +2,9 @@ package com.tkukulka.ztpai.manager.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(CorsConfig.class)
 @SpringBootApplication
 public class ZtpaiServiceApplication {
 
